725640a2521086600b6d75301006a1a6cc2364f3
Clemens Schlipfinger <clemens.schlipfinger@gmail.com>
2022-07-26 17:12:00 +0200

    added themeswitcher

---
+++ .config/desktop-themes/green/theme.hs
@@ -0,0 +1,14 @@
+--
+-- Xmonad Theme Cyber
+--
+
+module Theme where 
+
+accentColor :: String
+accentColor = "#03fc4e"
+
+accentLightColor :: String
+accentLightColor = "#fc034a"
+
+deactiveColor :: String
+deactiveColor = "#454545" --"#2b2b2b"
